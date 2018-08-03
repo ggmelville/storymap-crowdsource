0ad894551f52885c5638b44f307720db74bcfebc
Graham Melville <ggmelville@email.wm.edu>
2018-08-03 09:49:56 -0400

    Update Location.babel.js

--- src/app/components/forms/location/Location.babel.js
+++ src/app/components/forms/location/Location.babel.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import React from 'react'; // eslint-disable-line no-unused-vars
 import ReactDOM from 'reactDom';
 import Geocoder from 'esri/dijit/Geocoder';
-// import LocateButton from 'esri/dijit/LocateButton';
+import LocateButton from 'esri/dijit/LocateButton';
 import Locator from 'esri/tasks/locator';
 import lang from 'dojo/_base/lang';
 import webMercatorUtils from 'esri/geometry/webMercatorUtils';
@@ -171,7 +171,7 @@ export default class Location extends FormGroup {
         {this.props.tooltip ? <IconTooltip className="form-tooltip" {...this.props.tooltip} /> : null}
         <div
           className="geocoder"
-          ref={(ref) => this.geocoderContainer = ref}>
+          //ref={(ref) => this.geocoderContainer = ref}>
         </div>
         <div className="action-btn-wrapper">
         <div
